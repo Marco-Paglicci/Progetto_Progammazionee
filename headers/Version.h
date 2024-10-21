ae39568a101a0d14de462d2123dd3298a928a63b
Marco <marcopaglicci22@gmail.com>
2024-10-21 19:51:50 +0200

    Versione 1.1

---
+++ headers/Version.h
@@ -0,0 +1,13 @@
+//
+// Created by Marco on 21/10/2024.
+//
+
+#ifndef PROGETTO_PROGRAMMAZIONE_VERSION_H
+#define PROGETTO_PROGRAMMAZIONE_VERSION_H
+
+#include <string>
+
+static const std::string VERSION = "1.1";
+
+
+#endif //PROGETTO_PROGRAMMAZIONE_VERSION_H
